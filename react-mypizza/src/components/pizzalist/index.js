c0e8ae4d0d823fbc62ccf3bfe8a611e4ef435c22
Kevlin <kevlin.susanto@yahoo.com>
2021-01-25 17:33:31 +0100

    pizza list

--- react-mypizza/src/components/pizzalist/index.js
+++ react-mypizza/src/components/pizzalist/index.js
@@ -9,8 +9,13 @@ const PizzaList = () => {
             {data.map((pizza, index) => { 
                 return (
                 <ListContainer>
-                    <img src={pizza.image} width='100px' />     
-                    <h>{pizza.name}</h>
+                    <WrapperImg src={pizza.image} width='100px' />    
+                    <TittleDesc>  
+                    <PizzaTittle>{pizza.name}</PizzaTittle>
+                    <Desc>{pizza.description}</Desc>
+                    <Price>{pizza.price}€</Price>
+                    </TittleDesc> 
+                    <button>Ajouter</button>
                 </ListContainer>
                 ) 
             })}
@@ -18,16 +23,47 @@ const PizzaList = () => {
     );
 };
 const Wrapper = styled.div`
-
-`
-const ListContainer= styled.div`
 display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  column-count: 2;
-  column-gap: 0;
-  
+flex-direction: column;
+`
+const WrapperImg = styled.img`
+margin: 35px 20px 0px 10px;
+float: left;
+`
+const PizzaTittle = styled.h3`
+font-family: Takeaway Sans,Avant Garde,Century Gothic,Helvetica,Arial,sans-serif;
+font-weight: 600;
+font-size: 18px;
+line-height: 1.22;
+color: #0a3847;
+margin-bottom: 5px;
+`
+const Desc = styled.p`
+font-family: Takeaway Sans,Avant Garde,Century Gothic,Helvetica,Arial,sans-serif;
+font-weight: 400;
+font-size: 14px;
+color: #666;
+line-height: 1.5;
+`
+const Price = styled.p`
+font-family: Takeaway Sans,Avant Garde,Century Gothic,Helvetica,Arial,sans-serif;
+font-weight: 600;
+font-size: 18px;
+line-height: 1.22;
+letter-spacing: -.5px;
+color: #ff8000;
+margin: auto 0 0;
+`
 
+const TittleDesc = styled.div`
+    display: block;
+    margin: 30px 16px 10px 130px;
+`
+
+const ListContainer= styled.div`
+width-max:100%;
+border: 4px solid red;
+overflow: hidden;
 `
 
 
--- react-mypizza/src/screens/pizzas.js
+++ react-mypizza/src/screens/pizzas.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PizzaList from '../components/pizzalist'
 
 const Pizzas = () => {
     return (
         <div>
             <p>Liste des Pizzas !</p>
+            <PizzaList></PizzaList>
         </div>
     );
 };
+
 
 export default Pizzas;
