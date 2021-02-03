66d1d184c843c79529f4d28864273182e641df44
pollajoel <pollajoel2017@gmail.com>
2021-02-03 16:16:00 +0100

    cleaning

--- PIZZA/copie/teste2/react-mypizza/src/config/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore, applyMiddleware} from 'redux'
-import reducers from '../reducers'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-
-
-export const store = createStore(reducers, applyMiddleware(logger, thunk))
--- PIZZA/copie/teste2/react-mypizza/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import Cart from './cart'
-import Step from './stepper'
-
-export default combineReducers({
-    shopCart : Cart,
-    step:Step,
-})
