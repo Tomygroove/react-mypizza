72abfd179565e468436abf2e3b3b5c246cb4d839
Tomygroove <thomas.orard938@gmail.com>
2021-02-07 21:39:23 +0100

    fix

--- react-mypizza/src/components/header/mobile.js
+++ react-mypizza/src/components/header/mobile.js
@@ -5,21 +5,15 @@ import Logout from '../logout'
 import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import storage from 'local-storage-fallback'
 import toggleImage from '../../assets/images/themetoggle.png'
-import Mobile from "./mobile";
 
 
 import {useTranslation} from 'react-i18next'
 
-const Header = () => {
+const Mobile = () => {
     const {t, i18n} = useTranslation()
-
-    const [isMobile,setIsMobile] = useState(window.matchMedia('(max-width:768px)').matches);
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            setIsMobile(window.matchMedia('(max-width:768px)').matches);
-        })
-    })
     
+    const [active,setActive] = useState(false)
+
     const getInitialTheme = () => {
         const savedTheme = storage.getItem('theme')
         return savedTheme ? JSON.parse(savedTheme) : { mode: 'dark'}
@@ -36,16 +30,18 @@ const Header = () => {
           <ThemeProvider theme={theme}>
           <GlobalStyle/>
             <HeaderContainer>
-                <TitleLink to="/pizzas">My Pizza</TitleLink>
-                {isMobile ?
-                    <Mobile />
-                :
+
+                <MobileBtn onClick={() => {
+                    setActive(!active);
+                }}>
+                    {active ? "" : ""} Menu
+                </MobileBtn>
+                {active && 
+
                 <NavMenu>
-                    <Nav>
                         <NavLink to="/pizzas">{i18n.t('header.list')}</NavLink>
                         <NavLink to="/home">{i18n.t('header.configurator')}</NavLink>
                         <NavLink to="/cart">{i18n.t('header.panier')}</NavLink>
-                    </Nav>
                     <ToogleTheme onClick={e => setTheme(theme.mode === 'dark' ? {mode: 'light'} : {mode: 'dark'})}/>
                     <Logout />
                     <Langue>
@@ -54,8 +50,6 @@ const Header = () => {
                     </Langue>
                 </NavMenu>
                 }
-                
-                
             </HeaderContainer>
             </ThemeProvider>
         </>
@@ -106,37 +100,49 @@ cursor: pointer;
 `
 
 
-const HeaderContainer = styled.div`
-    height: 60px;
-    display: flex;
-    justify-content: space-between;
-    background: #191919;
-    padding: 0.5rem calc((100vw - 1000px) / 2);
-    z-index: 10;
-    font-family: 'Carter One', cursive;
+const MobileBtn = styled.button`
+    float: right;
+    margin-right: 15px;
+    margin-top: 20px;
+    color: white;
+    background: none;
+    border-radius:2px;
+    border:1px solid #d34836 ;
+    height: 3.5vh;
+    cursor: pointer;
+    &:hover{
+      background: #d34836;
+  }
 `
 
-const TitleLink = styled(Link)`
-color :#d34836;
-display: flex;
-align-items: center;
-text-decoration: none;
-padding: 0 1rem;
-cursor: pointer;
-&:hover {
-    color: white;
-}
+const HeaderContainer = styled.div`
+justify-content: center;
+flex-direction: column;
+life-style: none;
+width:100%;
+margin-left: 20%;
+background: #191919;
 `
 
 const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
-    margin-right: -24px;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+width:100%;
+margin-top: 0;
+margin-left: 0;
+background: #191919;
+height: auto;
 `
 
 const Nav = styled.div`
     display: flex;
-    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding-bottom: 10px;
+    width: 100%;
+    top: 50%;
 `
 
 const NavLink = styled(Link)`
@@ -144,12 +150,12 @@ color: #fff;
 display: flex;
 align-items: center;
 text-decoration: none;
+flex-direction: column;
 padding: 0 1rem;
-height: 100%;
 cursor: pointer;
 &.active {
     color: #d34836;
 }
 `
 
-export default Header
+export default Mobile
